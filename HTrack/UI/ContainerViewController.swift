92a78cdc6b28f29cb1565aaa177d23837092839b
Denis Shchigolev <denis.schigolev@gmail.com>
2021-08-11 18:44:46 +0300

    add ContainerViewController

---
+++ HTrack/UI/ContainerViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ContainerViewController.swift
+//  HTrack
+//
+//  Created by Jedi Tones on 8/9/21.
+//
+
+import UIKit
+
+class BaseContainerViewContoller: UIViewController {
+    var currentVC: UIViewController?
+    
+    var containerView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    func transitionToViewController(viewController: UIViewController, animated: Bool = true) {
+        guard viewController != currentVC else { return }
+        
+        currentVC?.willMove(toParent: nil)
+        
+        if !self.children.contains(viewController) {
+            self.addChild(viewController)
+        }
+        
+        self.addSubview(subView: viewController.view, toView: self.containerView)
+        
+        viewController.view.layoutIfNeeded()
+
+        func finishTransition() {
+            self.currentVC?.view.removeFromSuperview()
+            //self.currentVC?.removeFromParent()
+            
+            viewController.didMove(toParent: self)
+            
+            self.currentVC = viewController
+        }
+        
+        if animated {
+            viewController.view.alpha = 0
+            
+            UIView.animate(withDuration: Styles.Constants.animationDuarationBase,
+                           delay: 0,
+                           options: .transitionFlipFromLeft,
+                           animations: {
+                viewController.view.alpha = 1
+                self.currentVC?.view.alpha = 0
+            }) { (finished) in
+                finishTransition()
+            }
+        } else {
+            finishTransition()
+        }
+    }
+    
+    private func addSubview(subView: UIView, toView parentView: UIView) {
+        self.view.layoutIfNeeded()
+        parentView.addSubview(subView)
+        
+        subView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            subView.topAnchor.constraint(equalTo: parentView.topAnchor),
+            subView.bottomAnchor.constraint(equalTo: parentView.bottomAnchor),
+            subView.leadingAnchor.constraint(equalTo: parentView.leadingAnchor),
+            subView.trailingAnchor.constraint(equalTo: parentView.trailingAnchor)
+        ])
+    }
+}
